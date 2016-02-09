894f0050e3a4e1d72b5e393843d8c05674648a8f
erpy <oj.bae@erpyui-MacBook-Pro.local>
2016-02-09 18:17:21 +0900

    update

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
-cd /home/erpy/tomcat/bin && sh shutdown.sh
+cd /home/erpy/tomcat/bin
+sh shutdown.sh
+cd /home/erpy/BoardWangWeb/source/BoardWangWeb
 git fetch; git pull
 mvn clean; mvn compile; mvn package;
 rm -rf /home/erpy/tomcat/webapps/BoardWangWeb
 rm -rf /home/erpy/tomcat/webapps/BoardWangWeb.war
 cp -f /home/erpy/BoardWangWeb/source/BoardWangWeb/target/BoardWangWeb.war /home/erpy/tomcat/webapps
-cd /home/erpy/tomcat/bin && sh start.sh
+cd /home/erpy/tomcat/bin
+sh start.sh
